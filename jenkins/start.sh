5d995b5df79cf84bbca42c05e7d043d3c13f030e
libo <libo529@gmail.com>
2018-01-16 11:23:53 +0800

    update

--- jenkins/start.sh
+++ jenkins/start.sh
@@ -6,5 +6,4 @@ docker kill myjenkins|| true
 docker rm myjenkins || true
 docker run --name myjenkins -p 8080:8080 -ti -d -p 50000:50000 \
 --mount source=jenkins,target=/var/jenkins_home \
---env JAVA_OPTS="-Djava.util.logging.config.file=/var/jenkins_home/log.properties"\
   infisa/jenkins:v1
