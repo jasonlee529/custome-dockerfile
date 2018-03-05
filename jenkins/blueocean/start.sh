855559679b452772e9ddf17cf307d8cacabc9c94
libo <libo529@gmail.com>
2018-03-05 16:08:30 +0800

    update

--- jenkins/blueocean/start.sh
+++ jenkins/blueocean/start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-docker run \
-  --rm \
-  -u root \
-  -p 8082:8080 \
-  -v jenkins-data:/var/jenkins_home \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v "$HOME":/home \
-=======
 
 docker run \
   -u root \
@@ -17,5 +8,4 @@ docker run \
   -p 8080:8080 \
   -v jenkins-data:/var/jenkins_home \
   -v /var/run/docker.sock:/var/run/docker.sock \
->>>>>>> 119be152b7d1cc96af00b59949c5945a89f660e4
   jenkinsci/blueocean
