dc501e467a7125d17e5fbd883d875a2d58a61da9
jasonlee529 <libo529@gmail.com>
2018-03-14 22:53:53 +0800

    update

--- jenkins/jenkins-alpine/start.sh
+++ jenkins/jenkins-alpine/start.sh
@@ -3,4 +3,5 @@
 docker run -d -u root --name jenkins --rm \
     -p 8085:8080 -p 50000:50000 \
     -v jenkins_alpine_home:/var/jenkins_home \
-    jenkins:alpine
+     -v /var/run/docker.sock:/var/run/docker.sock \
+    jenkins/jenkins
