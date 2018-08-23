0b08bdedc8f0ea6a36b56611fd1a58637ba17b52
jason <libo529@gmail.com>
2018-08-23 12:01:36 +0800

    add kanboard

---
+++ kanboard/compose/config.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('ENABLE_URL_REWRITE') or define('ENABLE_URL_REWRITE', true);
+defined('LOG_DRIVER') or define('LOG_DRIVER', 'system');
+
+define('DB_DRIVER', 'mysql');
+
+// MySQL parameters
+define('DB_USERNAME', 'kanboard');
+define('DB_PASSWORD', 'kanboard');
+define('DB_HOSTNAME', '192.168.1.62');
+define('DB_PORT', '3306');
+define('DB_NAME', 'kanboard');
---
+++ kanboard/image/config.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('ENABLE_URL_REWRITE') or define('ENABLE_URL_REWRITE', true);
+defined('LOG_DRIVER') or define('LOG_DRIVER', 'system');
+
+define('DB_DRIVER', 'mysql');
+
+// MySQL parameters
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'sa');
+define('DB_HOSTNAME', '192.168.1.25');
+define('DB_NAME', 'kanboard');
