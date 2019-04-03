68fb207a3783edc3f09f81a7d7222bed296dfd26
jason <libo529@gmail.com>
2019-04-03 23:25:40 +0800

    update cnf file

--- database/mysql/slave/start.sh
+++ database/mysql/slave/start.sh
@@ -1,1 +1,7 @@
- docker run --name mysql-slave -p 3307:3306 -v $PWD:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7
+#!/bin/bash
+
+docker kill mysql-slave
+
+docker rm mysql-slave
+
+docker run --name mysql-slave -p 3307:3306 -v $PWD:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7
