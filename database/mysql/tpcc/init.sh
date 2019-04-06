10e3f3d22c2f2f4208c2ceacf5ed9ad9374d5fda
jason <libo529@gmail.com>
2019-04-06 21:21:30 +0800

    add files

--- database/mysql/tpcc/init.sh
+++ database/mysql/tpcc/init.sh
@@ -1,5 +1,8 @@
 # create database on your MySQL
+ mysql -uroot  -h 192.168.10.11 -p123456 -e "create database IF NOT EXISTS tpcc"
 # create tables and add indexes
-docker run -it --rm gaishimo/tpcc-mysql cat create_table.sql add_fkey_idx.sql | mysql -uroot  -h 192.168.10.11 -p123456 tpcc
+docker run -it --rm gaishimo/tpcc-mysql cat create_table.sql add_fkey_idx.sql | mysql -uroot  -h 192.168.10.11 -p123456 -D tpcc
 
-docker run -it --rm -e MYSQL_PORT_3306_TCP_ADDR=192.168.10.11 gaishimo/tpcc-mysql tpcc_load tpcc  root '123456' 10
+docker run -it --rm -e MYSQL_PORT_3306_TCP_ADDR=192.168.10.11 gaishimo/tpcc-mysql tpcc_load tpcc  root '123456' 1
+
+docker run -it --rm -e MYSQL_PORT_3306_TCP_ADDR=192.168.10.11 gaishimo/tpcc-mysql tpcc_start -d tpcc -u root -p123456 -w 1
