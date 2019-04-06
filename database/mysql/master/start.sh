508d09321f01c6b9240f22722fa0352b061839fe
jason <libo529@gmail.com>
2019-04-06 20:08:09 +0800

    udpate sh

--- database/mysql/master/start.sh
+++ database/mysql/master/start.sh
@@ -1,1 +1,1 @@
- docker run --name mysql-master -p 3306:3306 -v $PWD:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7
+docker run --name mysql-master -p 3306:3306 -v $PWD:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7
